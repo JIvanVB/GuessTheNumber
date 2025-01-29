4c64b644d6548c7390b049153e06a32088bb1663
jivb <darksamus0997@hotmail.com>
2025-01-28 22:58:40 -0700

    no se si la funcionalidad se logro, pero corre sin problemas

--- app/src/main/java/com/example/guessthenumber/MainActivity.kt
+++ app/src/main/java/com/example/guessthenumber/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     var min=0
     var max=100
     var num=0
+    var won=false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,15 +27,16 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val gues:TextView=findViewById(R.id.tap)
-        val down:Button=findViewById(R.id.down)
-        val up:Button=findViewById(R.id.up)
-        val gene:Button=findViewById(R.id.gene)
-        val guessed:Button=findViewById(R.id.guessed)
+        val tap:TextView=findViewById(R.id.tap)
+        val down    :Button=findViewById(R.id.down)
+        val up      :Button=findViewById(R.id.up)
+        val gene    :Button=findViewById(R.id.gene)
+        val guessed :Button=findViewById(R.id.guessed)
+        guessed.visibility=View.INVISIBLE
 
         gene.setOnClickListener {
             num = Random.nextInt(min,max)
-            gues.text=num.toString()
+            tap.text=num.toString()
             gene.visibility= View.INVISIBLE
             guessed.visibility=View.VISIBLE
         }
@@ -43,28 +45,41 @@ class MainActivity : AppCompatActivity() {
             min=num
             if (checkingLimits()) {
                 num = Random.nextInt(min, max)
-                gues.text = num.toString()
-            } else gues.text = "Ganaste"
+                tap.text = num.toString()
+            } else tap.text = "Ganaste"
         }
 
         down.setOnClickListener {
             max=num
             if (checkingLimits()) {
                 num = Random.nextInt(min, max)
-                gues.text = num.toString()
-            } else gues.text = "Ganaste"
+                tap.text = num.toString()
+            } else tap.text = "Ganaste"
         }
 
         guessed.setOnClickListener {
-            gues.text = "tu numero es: ${num}"
-            
+            if(!won) {
+                tap.text = "Adivine, tu numero es: ${num}"
+                guessed.text = "Volver a jugar"
+                tap.text = "Tap on generate"
+                won = true
+            }else {
+                gene.visibility = View.VISIBLE
+                tap.text="Tap on generate"
+                guessed.visibility=View.INVISIBLE
+                resetValues()
+            }
 
         }
-
-
-
-
     }
+
+    private fun resetValues() {
+        min=0
+        max=100
+        num=0
+        won=false
+    }
+
     fun checkingLimits():Boolean{
         return min != max
     }
